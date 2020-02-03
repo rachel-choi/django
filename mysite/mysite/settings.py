84de5c976c184a9170582301f62081582902aa7b
Rachel C <yunjincg>
2020-02-03 06:06:40 -0500

    add stuff

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import django_heroku
+import dj_database_url
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 django_heroku.settings(locals())
@@ -24,9 +26,9 @@ django_heroku.settings(locals())
 SECRET_KEY = 'dipzif8q^iz0fw&0xfmi1#_#idk+sez2xwutkc5up@s&2v880p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
