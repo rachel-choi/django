9a07aa6388f825b61b211d663273aa921fdf0c11
Rachel C <yunjincg>
2020-02-03 04:29:34 -0500

    update procfile

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -14,11 +14,7 @@ import os
 import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-# django_heroku.settings(locals())
-
-if '/app' in os.environ['HOME']:
-    import django_heroku
-    django_heroku.settings(locals())
+django_heroku.settings(locals())
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
